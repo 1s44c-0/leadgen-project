0bdcfa0de8405f90df5c4934e0b68eaf1753f9f3
Isaac Olorunfemi <isaacolorunfemi.dev@gmail.com>
2026-09-17 14:06:26 -0700

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -18,7 +18,7 @@ from .serializers import OrderSerializer
 # PRODUCT PRICE
 # ============================================================
 
-PRODUCT_PRICE = 1500  # ₦1,500 per unit
+PRODUCT_PRICE = 2000  # ₦2,000 per unit
 
 
 # ============================================================
